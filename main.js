87ec74ffc963b15a7088bb8b409cc40e42e98299
MatteoSanson <matteo.sanson@gmail.com>
2023-11-13 17:25:03 +0100

    feat: ciclo con for

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('sto funzionando');
+'use strict';
+
+const lista = document.querySelector('.lista');
+
+const spesa = ['pane','latte','mele','pere','farina','uova','pasta'];
+console.log (spesa);
+
+
+// ciclo con for 
+for (let i=0; i <= spesa.length - 1; i++) {
+    console.log(spesa[i]);
+
+    const articolo = document.createElement('li');
+    lista.append(articolo);
+    articolo.textContent = spesa[i];
+}
